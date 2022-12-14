d223405d4abf4bb455b948e2bb23f64d2850f6cb
Emmy <esokel116@outlook.com>
2022-12-14 04:23:01 -0800

    modified main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,4 +3,5 @@
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
+void print_times_table(int n);
 void print_remaining_days(int month, int day, int year);
