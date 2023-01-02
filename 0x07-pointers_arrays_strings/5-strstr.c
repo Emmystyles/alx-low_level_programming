8e517ea5c4e0be37b60475686c737129b372f827
Emmy <esokel116@outlook.com>
2023-01-02 11:57:46 -0800

    Created 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,47 +1,36 @@
 #include "main.h"
 #define NULL 0
-
-
 /**
- * _strstr -  a function that locates a substring.
- * @haystack: an input string to search in
- * @needle: an input string to locate into string haystack
- * Return:  a pointer to the beginning of the located substring,
- * or NULL if the substring is not found.
+ * _strstr - locate and return pointer to first occurence of substring
+ * @haystack: string to search
+ * @needle: target substring to search for
+ * Return: pointer to index of string at first occurence of whole substring
  */
-
 char *_strstr(char *haystack, char *needle)
-
 {
-
-char *startn = needle, *starth = haystack;
-
-while (*haystack)
-
-{
-
-starth = haystack;
-
-needle = startn;
-
-while (*haystack == *needle)
-
-{
-
-haystack++;
-
-needle++;
-
-}
-
-if (*needle == '\0')
-
+int i = 0, j, x;
+if (needle[0] == '\0')
 return (haystack);
-
-haystack = starth + 1;
-
+while (haystack[i] != '\0') /* iterate through haystack */
+{
+/* if a byte matches first char of needle */
+/* interate through needle until match ends */
+if (haystack[i] == needle[0])
+{
+x = i, j = 0;
+while (needle[j] != '\0')
+{
+if (haystack[x] == needle[j])
+x++, j++;
+else
+break;
+} /* if matched throughout, return haystack */
+if (needle[j] == '\0')
+{
+return (haystack + i);
 }
-
-return (NULL);
-
+}
+i++;
+}
+return (NULL); /* No match */
 }
