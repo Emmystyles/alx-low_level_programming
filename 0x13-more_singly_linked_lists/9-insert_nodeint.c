537e1c4e82720a2a14cbc1adc79c90d997c0e40a
Emmy <esokel116@gmail.com>
2023-01-31 11:48:44 -0800

    created 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,41 +1,42 @@
 #include "lists.h"
-
 /**
- * insert_nodeint_at_index - This function inserts a
- * new node at given position
- * @head: The head node
- * @idx: The index of the list where new node is to be added
- * @n: The integer data of the new node
- * Return: The address of the new inserted node or NULL if failed
+ * insert_nodeint_at_index - A function that inserts node at given index
+ * @head: A pointer to listint_t structure
+ * @idx: The index of the list
+ * @n: An integer data for new node
+ * Return: The address to new node at specified index, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *node, *temp = *head;
-size_t i = 0;
-node = malloc(sizeof(listint_t));
-if (!node)
+unsigned int i = 1;
+listint_t *new_node, *temp;
+if (head == NULL)
 return (NULL);
-node->n = n;
-node->next = NULL;
-if (!*head && !idx)
-{
-*head = node;
-return (node);
-}
-else if (!*head && idx)
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
 return (NULL);
-else if (!idx)
+if (*head == NULL)
 {
-node->next = temp;
-*head = node;
-return (node);
+*head = new_node;
+new_node->next = NULL;
+new_node->n = n;
+return (new_node);
 }
-while (i < (idx - 1))
+if (idx == 0)
+{
+new_node->next = *head;
+new_node->n = n;
+*head = new_node;
+return (new_node);
+}
+temp = *head;
+while (i < idx)
 {
 temp = temp->next;
 i++;
 }
-node->next = temp->next;
-temp->next = node;
-return (node);
+new_node->n = n;
+new_node->next = temp->next;
+temp->next = new_node;
+return (new_node);
 }
